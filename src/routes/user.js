7887398152c4cf375f4b53f4865e0ba0f82f900a
Ankit Karn <karnankit3954@gmail.com>
2026-02-20 11:57:10 +0530

    added user conncetions routes

---
+++ src/routes/user.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequestModel = require("../models/connectionRequest");
+const userRouter = express.Router();
+
+const USER_SAFE_DATA = "firstName lastName skills photoUrl about"
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequestsRecieved = await ConnectionRequestModel.find({
+            toUserId: loggedInUser._id,
+            status: 'interested'
+        }).populate('fromUserId', USER_SAFE_DATA)
+
+        if (!connectionRequestsRecieved) {
+            return res.status(404).json({
+                message: "No requests Found"
+            })
+        }
+
+        res.json({
+            message: "Data fetched successfully",
+            data: connectionRequestsRecieved,
+        });
+
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+})
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ],
+        })
+            .populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+})
+
+module.exports = userRouter;
