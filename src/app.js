b21947bd3581a27cb404861309800f0c5ff4b0fd
Ankit Karn <karnankit3954@gmail.com>
2026-02-26 12:15:16 +0530

    added cors config

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const { connectDB } = require("../src/config/database")
-const { User } = require("./models/user")
 const app = express();
-const { validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("./middlewares/auth");
+const cors = require("cors")
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser());
 
