d70b59d96d55f23d1e1ecb82acc87737384cce7f
Ankit Karn <karnankit3954@gmail.com>
2026-03-17 10:42:31 +0530

    added chat feature

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { connectDB } = require("../src/config/database")
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
+const http = require("http")
 
 require('dotenv').config({ path: "./src/.env" });
 require("./utils/cronJobs")
@@ -12,6 +13,11 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment");
+const chatRouter = require("./routes/chat");
+const intializeSocket = require("./utils/socket");
+
+const server = http.createServer(app);
+intializeSocket(server)
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -27,12 +33,13 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
 
 connectDB()
     .then(() => {
         console.log("Database connection established");
-        app.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT, () => {
             console.log("Server is listening on 3000");
         });
     })
